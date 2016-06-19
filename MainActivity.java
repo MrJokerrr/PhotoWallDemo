24884c8becb9209e637a954d0628b9b250b90561
MrJokerrr <510327167@qq.com>
2016-06-20 00:18:22 +0800

    xml add GridView

--- MainActivity.java
+++ MainActivity.java
@@ -1,15 +1,18 @@
 package com.masai.photowalldemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridView mGv_photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        mGv_photo = (GridView) findViewById(R.id.gv_photo);
     }
 }
